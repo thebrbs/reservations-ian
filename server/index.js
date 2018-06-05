756d20fdc716e049d38b223facd095c77cd0f639
Andrew <andrew@MacBook-Pro-4.local>
2018-06-04 17:34:34 -0700

    React Component skeleton in progress recommit

--- server/index.js
+++ server/index.js
@@ -3,8 +3,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var app = express();
+
 app.use(express.static(path.join(__dirname, '../public')));
 app.get('/restaurant/:restaurant_id', function(req, res) {
 	res.send(req.params);
-})
+});
+
 app.listen(3000);
