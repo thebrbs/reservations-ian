5a8cc5d254cef1d5105b0bb66bb784034acab56d
Andrew <andrew@MacBook-Pro-4.local>
2018-06-07 16:46:43 -0700

    shifted module left

--- client/components/partySize.jsx
+++ client/components/partySize.jsx
@@ -4,7 +4,7 @@ const partySize = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18
 const PartySize =(props) => {
   return (
     <div>
-      <div>Party Size</div>
+      <div id="partyTitle">Party Size</div>
       <select onChange={props.partyChange} className="opts" id="PartySize">
         {partySize.map((size, id) => (
           <option value={size} key={id} >{size}</option>
