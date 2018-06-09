e72e911eb2741e90e9697493897ce3feaf2eb9d8
Andrew <andrew@MacBook-Pro-4.local>
2018-06-08 21:20:14 -0700

    fixed a bug in calendar

--- client/components/date.jsx
+++ client/components/date.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Date = (props) => {
   return (
-    <input onChange={props.dateChange} type="date" id="calendar" value="2018-05-31" ></input>
+    <input onChange={props.dateChange} type="date" id="calendar" ></input>
   ) 
 }
 
