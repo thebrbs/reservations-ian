b6c5e19176ad2c93b87840b85bacd05741937b80
Andrew Liu <andrewliu@Andrews-MacBook-Pro-3.local>
2018-06-13 16:03:18 -0700

    restyle complete with cssmodule and BOOTSTRAP

--- client/components/partySize.jsx
+++ client/components/partySize.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import pt from './partySize.css';
+
 
 const partySize = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 const PartySize =(props) => {
   return (
     <div> 
-      <div id="partyTitle">Party Size</div>
-        <span id='arrowptsize' className="glyphicon glyphicon-chevron-down"></span>
-        <span id="for">For</span>
-        <div id="partyD">
-          <select onChange={props.partyChange} className="partySelect" id="PartySize">
+      <div className={`${pt.partyTitle}`} >Party Size</div>
+        <span className={`${pt.arrowptsize} glyphicon glyphicon-chevron-down`}></span>
+        <span className={`${pt.for}`}>For</span>
+        <div className={`${pt.partyD}`}>
+          <select onChange={props.partyChange} className={`${pt.partySelect} ${pt.PartySize}`}>
             {partySize.map((size, id) => (
               <option value={size} key={id} >&#160;&#160;&#160;&#160;&#160;{size}</option>
               )
