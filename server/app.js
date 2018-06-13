3e6e02b4b46586727779b1f31b653ccc7779bb05
Andrew Liu <andrewliu@Andrews-MacBook-Pro-3.local>
2018-06-13 16:07:58 -0700

    removed comments and console.logs

--- server/app.js
+++ server/app.js
@@ -3,11 +3,6 @@ const express = require('express');
 const db = require('../database/db.js');
 
 let app = express();
-app.use('/', function(req,res,next) {
-  console.log(req.url);
-  next();
-})
-// app.use('/restaurant/:restaurant_id/bootstrap.min.css', express())
 app.use('/restaurant/:restaurant_id', express.static(path.join(__dirname, '../public')));
 
 app.get('/restaurant/:restaurant_id/:date', function(req, res) {
