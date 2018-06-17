a3e0fa0f9765b57375f2abc62b80d595e5a9aa01
Andrew Liu <andrewliu@Andrews-MacBook-Pro-3.local>
2018-06-17 14:02:08 -0700

    reservations 1.0.1

--- database/config.js
+++
@@ -1,9 +0,0 @@
-var key = {
-  endpoint: process.env.RDS_HOSTNAME,
-  rdsport: process.env.RDS_PORT,
-  database: process.env.RDS_DB_NAME,
-  user: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD
-};
-
-module.exports = key; 
