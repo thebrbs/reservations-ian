8118f82971b84826172eb657e2d9de9366f4e39b
Andrew Liu <andrewliu@Andrews-MacBook-Pro-3.local>
2018-06-17 14:18:16 -0700

    removed redunant files

--- reservationsN1.0/README.md
+++
@@ -1,3 +0,0 @@
-# andrew-service
-Reservation module in https://www.opentable.com/sons-and-daughters?p=2&sd=2018-06-09+21%3A00&page=1.
-
--- reservationsN1.0/server/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app.js')
-
-const port = process.env.PORT || 3001;
-
-app.listen(port, function() {
-  console.log("Listening to port 3001");
-});
-
- 
