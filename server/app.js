2d07a4287d2c47f5b93bca30087292bf1d7ecf8f
i-mckinney <baseballrocks@gmail.com>
2018-06-19 20:35:32 -0700

    added CRUD

--- server/app.js
+++ server/app.js
@@ -11,9 +11,39 @@ app.get('/restaurant/:restaurant_id/:date', function(req, res) {
     if (error) {
       res.sendStatus(500);
     }
-    res.send(data); 
+    res.status(200).send(data); 
   }); 
 });  
+
+app.post('/restaurant/:restaurant_id/:date/:time', function(req, res) {
+  db.addTimeSlot(req.params.restaurant_id, req.params.date, req.params.time, function(error, data) {
+    if (error) {
+      res.sendStatus(500)
+    } else {
+      res.status(201)
+    }
+  })
+})
+
+app.put('/restaurant/:restaurant_id/:date', function(req, res) {
+  db.updateTimeSlot(req.params.restaurant_id, req.params.date, function(error, data) {
+    if (error) {
+      res.sendStatus(400)
+    } else {
+      res.sendStatus(202)
+    }
+  })
+})
+
+app.delete('/restaurant/:restaurant_id/:date/:time', function(req, res) {
+  db.deleteTimeSlot(req.params.restaurant_id, req.params.date, req.params.time, function(error, data) {
+    if (error) {
+      res.sendStatus(400)
+    } else {
+      res.sendStatus(204)
+    }
+  })
+})
    
 module.exports = app;
  
